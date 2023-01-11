fbfae0b2ac4cef6ff2903df2792c20645d6ff7f1
Arnoud de Boer <a.deboer@geonovum.nl>
2023-01-11 15:27:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,26 @@ var respecConfig = {
   specType: "IM",
   //canonicalURI: "VV",
   //publishDate: "2022-09-01",
+ maxTocLevel: 4,
+ logos: [{
+    src: 'https://docs.geostandaarden.nl/bro/gen/logo_minbzk.png',
+    href: "https://www.geobasisregistraties.nl/basisregistraties/ondergrond",
+    alt: "Ministerie BZK",
+    width: 776,
+    height: 163,
+    id: 'logo_minbzk',
+  }] ,	
   editors: [
     {
-      name: "TNO Geologische Dienst Nederland",
-      url: "https://www.tno.nl/nl/",
-    },
+      name: "Geonovum",
+      url: "https://www.geonovum.nl", 
+    },  
   ],
+  authors: [
+    {
+      name: "Geonovum",
+	  url: "https://www.geonovum.nl",
+	}],
   shortName: "CPT",
   pubDomain: "bro",
   github: "https://github.com/BROprogramma/CPT",
